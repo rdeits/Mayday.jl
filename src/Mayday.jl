00400a308605ce536285b297f71326eac9250da6
Robin Deits <robin.deits@gmail.com>
2016-02-03 15:12:20 -0500

    import old SoS package

--- src/Mayday.jl
+++ src/Mayday.jl
@@ -1,5 +1,96 @@
 module Mayday
 
-# package code goes here
+using JuMP
+using MultiPoly
+import MultiPoly: print
+import Base: dot
+import DataStructures: OrderedDict
+export monomials, 
+	   generator,
+	   generators, 
+	   evaluate,
+	   defSoSPolynomial, 
+	   addPolynomialEqualityConstraint,
+	   addSoSConstraint
 
-end # module
+generators(s::Symbol...) = MultiPoly.generators(MPoly{Float64}, s...)
+generator(s::Symbol) = generators(s)[1]
+
+function print{T<:JuMP.GenericAffExpr}(io::IO, p::MPoly{T})
+    first = true
+    for (m, c) in p
+        if first
+            print(io, c)
+            MultiPoly.printmonomial(io, m, vars(p))
+            first = false
+        else
+            print(io, " + ($c)")
+            MultiPoly.printmonomial(io, m, vars(p))
+        end
+    end
+    if first
+        print(io, zero(T))
+    end
+end
+
+function monomials(variables, degrees=0:2)
+    sort!(degrees)
+    monomials = Array(MPoly{Float64}, 0)
+    powers = zeros(Int64, length(variables))
+    total = 0
+    terms = OrderedDict(powers=>1.0)
+    while powers[1] <= degrees[end]
+        for d = degrees
+            if total == d
+            	push!(monomials, MPoly{Float64}(terms, variables))
+                break
+            end
+        end
+        powers[end] += 1
+        total += 1
+        for j = length(powers):-1:2
+            if powers[j] > degrees[end] || total > degrees[end]
+                total -= (powers[j] - 1)
+                powers[j] = 0
+                powers[j-1] += 1
+            else
+                break
+            end
+        end
+    end
+    return monomials
+end
+
+dot(x::Number, y::MPoly) = x * y
+dot(x::MPoly, y::Number) = x * y
+dot(x::MPoly, y::JuMP.GenericAffExpr) = x*y
+dot(x::JuMP.GenericAffExpr, y::MPoly) = x*y
+
+function defSoSPolynomial{T}(model::JuMP.Model, variables::Vector{Symbol}, basis::Vector{MPoly{T}})
+    @defVar(model, Q[1:length(basis), 1:length(basis)], SDP)
+    return sum(sum(basis .* Q, 1)' .* basis)
+end
+
+defSoSPolynomial(model::JuMP.Model, variables::Vector{Symbol}, max_monomial_degree::Integer) = defSoSPolynomial(model, variables, monomials(variables, 0:max_monomial_degree))
+
+function addPolynomialEqualityConstraint(model::JuMP.Model, poly1::MPoly, poly2::MPoly)
+    @assert poly1.vars == poly2.vars
+    constrained_powers = []
+    for (powers, coefficient) in poly1
+        @addConstraint(model, poly2[powers] == coefficient)
+        push!(constrained_powers, powers)
+    end
+    for (powers, coefficient) in poly2
+        if !in(powers, constrained_powers)
+            @addConstraint(model, poly1[powers] == coefficient)
+        end
+    end
+end
+
+function addSoSConstraint(model, poly)
+    max_degree = maximum([maximum(x) for x in keys(poly.terms)])
+    sos_poly = defSoSPolynomial(model, poly.vars, max_degree)
+    addPolynomialEqualityConstraint(model, sos_poly, poly)
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,41 @@
 using Mayday
+using JuMP
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+function test_simple_sos_problem()
+	# Let's find a polynomial which is positive for x < -1 and negative for x > 1
+	# v(x) + d1(x) * (x + 1) is SOS
+	# -v(x) + d2(x) * (1 - x) is SOS
+	# d1(x) is SOS
+	# d2(x) is SOS
+
+	m = Model()
+	degree = 4
+	x = generator(:x)
+	monos = monomials([:x], 0:degree)
+	@defVar(m, coeffs[1:length(monos)])
+	v = sum(coeffs .* monos)
+	@show v
+	d1 = defSoSPolynomial(m, [:x], degree)
+	d2 = defSoSPolynomial(m, [:x], degree)
+	addSoSConstraint(m, v + d1 * (x + 1))
+	addSoSConstraint(m, -v + d2 * (1 - x))
+	solve(m)
+
+	# Extract the result:
+	poly = sum(map(getValue, coeffs) .* monos)
+
+	# Verify it:
+	x = linspace(-2, 2)
+	y = map(x -> evaluate(poly, x), x)
+	for i = 1:length(x)
+		if x[i] < -1
+			@test y[i] >= 0
+		elseif x[i] > 1
+			@test y[i] <= 0
+		end
+	end
+end
+
+test_simple_sos_problem()
+
